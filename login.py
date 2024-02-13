8eead059371b4a3edf0b437af068fae22be789bc
al-11-k <allykarlis@gmail.com>
2024-02-12 21:44:40 -0800

    Initial Commit

---
+++ login.py
@@ -0,0 +1,92 @@
+#not a microservice
+import hashlib
+import re
+import time
+
+
+def signup():
+    print("Welcome! We are so glad that you are signing up!")
+    while True:
+        username = input("Enter your username or email address: ")
+        check = open('credential.txt', 'r')
+        opened_check = check.read()
+        match = re.search(username, opened_check)
+
+        if match is not None:
+            print("Your email or username is already registered with an account! Please select the login option.")
+            check.close()
+            time.sleep(3)
+            return(1)
+        pwd = input("Enter your password: ")
+        conf_pwd = input("Confirm your password: ")
+
+        if conf_pwd != pwd:
+            print("Oh-oh! Your password is not the same as above! Please try again!\n")
+            continue
+
+        else:
+            enc = conf_pwd.encode()
+            hash1 = hashlib.md5(enc).hexdigest()
+
+            database = open("credential.txt", "w")
+            database.write(username + "\n")
+            database.write(hash1)
+            database.close()
+
+            print("You have successfully registered!")
+            return(0)
+
+
+def login():
+    while True:
+        email = input("Enter email: ")
+        pwd = input("Enter your password: ")
+
+        authenticate = pwd.encode()
+        authenticated_hash = hashlib.md5(authenticate).hexdigest()
+        database = open("credential.txt", "r")
+        stored_email, stored_pwd = database.read().split("\n")
+        database.close()
+
+        if email == stored_email and authenticated_hash == stored_pwd:
+            print("You have successfully logged in!")
+            return(0)
+        else:
+            if email != stored_email and authenticated_hash == stored_pwd:
+                print("Login failed. You entered an incorrect email. Please try again.")
+
+            if email == stored_email and authenticated_hash != stored_pwd:
+                print("Login failed. Password not valid. Please try again.")
+
+            else:
+                print("Login failed. Both email and password and incorrect. Are you sure you have an account with us? If not, create one with the sign in option.")
+                return(1)
+
+
+def login_process():
+    while True:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("\n                             Welcome! Let's login or create a new account!\n")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("1. Signup")
+        print("2. Login")
+        print("3. Exit")
+        print("4. Help")
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            ret = signup()
+            if ret != 1:
+                return 0
+
+        if option == 2:
+            ret = login()
+            if ret != 1:
+                return 0
+        if option == 3:
+            return 1
+        if option == 4:
+            pass
+
+
+
+
---
+++ scraper.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+#add to url to accomodate for equiptment
+
+def scraper(level, style, time, muscles):
+    print("Searching for the perfect exercises!")
+    if muscles == "random":
+        if style == "random":
+            url = "https://bodybuilding.com/exercises/finder/?level="f"{level},beginner"
+        else:
+            url = "https://bodybuilding.com/exercises/finder/?exercise-type="f"{style}&level="f"{level},beginner"
+    else:
+        for muscle in muscles:
+            if style == "random":
+                url = "https://bodybuilding.com/exercises/finder/?exercise-type="f"{level},beginner&muscle="f"{muscle}"
+            else:
+                url = "https://bodybuilding.com/exercises/finder/?exercise-type="f"{style}&level="f"{level},beginner&muscle="f"{muscle}"
+
+            request = requests.get(url)
+            soup = BeautifulSoup(request.content, "html.parser")
+            exercises = soup.find_all("div", class_="ExResult-cell ExResult-cell--nameEtc")
+            f = open("exercises.txt", "w+")
+            for exercise in exercises:
+                name = exercise.find("h3", class_="ExHeading ExResult-resultsHeading")
+                f.write(name.text.strip() + '\n')
+            f.close()
+
+
